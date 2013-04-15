1630c6da57a19a94f0a2a61902201d8909aa03c8
Derak Berreyesa <derakberreyesa@gmail.com>
2013-04-15 14:23:09 -0700

    Initial Commit

---
+++ Editor/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Editor
+//
+//  Created by Derak Berreyesa on 4/15/13.
+//  Copyright (c) 2013 Berreyesa Media LLC. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
